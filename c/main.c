966c489042fb40a9d8d6eda85b4864f92b5b9525
10xdev4u-alt <10xdev4u@gmail.com>
2026-02-25 00:08:28 +0530

    c: Connect Parser to REPL loop

--- c/main.c
+++ c/main.c
@@ -3,46 +3,15 @@
 #include <string.h>
 #include "lexer.h"
 #include "token.h"
+#include "ast.h"
+#include "parser.h"
 
 #define MAX_INPUT 1024
-
-const char* token_type_to_string(TokenType type) {
-    switch (type) {
-        case TOKEN_ILLEGAL: return "ILLEGAL";
-        case TOKEN_EOF: return "EOF";
-        case TOKEN_IDENT: return "IDENT";
-        case TOKEN_INT: return "INT";
-        case TOKEN_ASSIGN: return "ASSIGN";
-        case TOKEN_PLUS: return "PLUS";
-        case TOKEN_MINUS: return "MINUS";
-        case TOKEN_BANG: return "BANG";
-        case TOKEN_ASTERISK: return "ASTERISK";
-        case TOKEN_SLASH: return "SLASH";
-        case TOKEN_LT: return "LT";
-        case TOKEN_GT: return "GT";
-        case TOKEN_EQ: return "EQ";
-        case TOKEN_NOT_EQ: return "NOT_EQ";
-        case TOKEN_COMMA: return "COMMA";
-        case TOKEN_SEMICOLON: return "SEMICOLON";
-        case TOKEN_LPAREN: return "LPAREN";
-        case TOKEN_RPAREN: return "RPAREN";
-        case TOKEN_LBRACE: return "LBRACE";
-        case TOKEN_RBRACE: return "RBRACE";
-        case TOKEN_FUNCTION: return "FUNCTION";
-        case TOKEN_LET: return "LET";
-        case TOKEN_TRUE: return "TRUE";
-        case TOKEN_FALSE: return "FALSE";
-        case TOKEN_IF: return "IF";
-        case TOKEN_ELSE: return "ELSE";
-        case TOKEN_RETURN: return "RETURN";
-        default: return "UNKNOWN";
-    }
-}
 
 int main() {
     char buffer[MAX_INPUT];
     
-    printf("REPLngne (C Edition) - v0.1.0\n");
+    printf("REPLngne (C Edition) - v0.2.0 (Parser Active)\n");
     printf("Type 'exit' to quit.\n");
 
     while (1) {
@@ -59,16 +28,22 @@ int main() {
         }
 
         Lexer *l = new_lexer(buffer);
-        
-        Token tok;
-        do {
-            tok = next_token(l);
-            printf("{Type: %s, Literal: %s}\n", token_type_to_string(tok.type), tok.literal);
-            if (tok.literal) {
-                free(tok.literal);
-            }
-        } while (tok.type != TOKEN_EOF);
+        Parser *p = new_parser(l);
+        Program *program = parse_program(p);
 
+        if (p->error_count > 0) {
+            printf("Parser errors:\n");
+            for (int i = 0; i < p->error_count; i++) {
+                printf("\t%s\n", p->errors[i]);
+            }
+        } else {
+            char *prog_str = program->base.string((ASTNode*)program);
+            printf("%s\n", prog_str);
+            free(prog_str);
+        }
+
+        program->base.free((ASTNode*)program);
+        free_parser(p);
         free_lexer(l);
     }
 
