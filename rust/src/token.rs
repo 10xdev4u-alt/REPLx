7ed51d0bfaa9c269298387302f7a5a3a14ffb8d0
10xdev4u-alt <10xdev4u@gmail.com>
2026-02-24 23:44:51 +0530

    rust: Define Token enum and keywords

---
+++ rust/src/token.rs
@@ -0,0 +1,51 @@
+#[derive(Debug, PartialEq, Clone)]
+pub enum Token {
+    // Keywords
+    Let,
+    Fn,
+    If,
+    Else,
+    Return,
+    True,
+    False,
+
+    // Identifiers + Literals
+    Ident(String),
+    Int(i64),
+    String(String),
+
+    // Operators
+    Assign,
+    Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+    
+    // Delimiters
+    Comma,
+    Semicolon,
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+
+    // End of File
+    Illegal,
+    EOF,
+}
+
+impl Token {
+    pub fn lookup_ident(ident: &str) -> Token {
+        match ident {
+            "fn" => Token::Fn,
+            "let" => Token::Let,
+            "true" => Token::True,
+            "false" => Token::False,
+            "if" => Token::If,
+            "else" => Token::Else,
+            "return" => Token::Return,
+            _ => Token::Ident(ident.to_string()),
+        }
+    }
+}
