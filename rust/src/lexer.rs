57539a1c4cf5d5f470ee366e1a602672ae109391
10xdev4u-alt <10xdev4u@gmail.com>
2026-02-24 23:45:52 +0530

    rust: Implement Lexer struct and token scanning

---
+++ rust/src/lexer.rs
@@ -0,0 +1,97 @@
+use crate::token::Token;
+
+pub struct Lexer {
+    input: Vec<char>,
+    position: usize,
+    read_position: usize,
+    ch: char,
+}
+
+impl Lexer {
+    pub fn new(input: String) -> Lexer {
+        let mut l = Lexer {
+            input: input.chars().collect(),
+            position: 0,
+            read_position: 0,
+            ch: '\0',
+        };
+        l.read_char();
+        l
+    }
+
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = '\0';
+        } else {
+            self.ch = self.input[self.read_position];
+        }
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    fn peek_char(&self) -> char {
+        if self.read_position >= self.input.len() {
+            '\0'
+        } else {
+            self.input[self.read_position]
+        }
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.ch.is_whitespace() {
+            self.read_char();
+        }
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+
+        let tok = match self.ch {
+            '=' => Token::Assign,
+            '+' => Token::Plus,
+            '-' => Token::Minus,
+            '!' => Token::Bang,
+            '*' => Token::Asterisk,
+            '/' => Token::Slash,
+            ',' => Token::Comma,
+            ';' => Token::Semicolon,
+            '(' => Token::LParen,
+            ')' => Token::RParen,
+            '{' => Token::LBrace,
+            '}' => Token::RBrace,
+            '\0' => Token::EOF,
+            _ => {
+                if self.ch.is_alphabetic() || self.ch == '_' {
+                    let literal = self.read_identifier();
+                    return Token::lookup_ident(&literal);
+                } else if self.ch.is_numeric() {
+                    let literal = self.read_number();
+                    // Assuming i64 for simplicity
+                    return Token::Int(literal.parse().unwrap());
+                } else {
+                    Token::Illegal
+                }
+            }
+        };
+
+        self.read_char();
+        tok
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let position = self.position;
+        while self.ch.is_alphabetic() || self.ch == '_' {
+            self.read_char();
+        }
+        // Correctly slice the char vector
+        self.input[position..self.position].iter().collect()
+    }
+
+    fn read_number(&mut self) -> String {
+        let position = self.position;
+        while self.ch.is_numeric() {
+            self.read_char();
+        }
+        self.input[position..self.position].iter().collect()
+    }
+}
